56bff258ab2ceeb08a6979b95bd052ccea01f000
Safaet-ullah <shafetullah02@gmail.com>
2024-05-20 05:06:49 +0600

    script work finally done

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -1,136 +1,121 @@
-""" 
-Contains functionfor training and testing a pytorch model
 """
-from typing import Dict,List,Tuple
+Contains functions for training and testing a PyTorch model.
+"""
+from typing import Dict, List, Tuple
+
 import torch
+
 from tqdm.auto import tqdm
-import torch.utils.data
 
-def train_step(model : torch.nn.Module,
+def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
-               loss_fn : torch.nn.Module,
+               loss_fn: torch.nn.Module,
                optimizer: torch.optim.Optimizer,
-               device: torch.device)-> Tuple[float,float]:
-    
-    """ Trains a pyotrch model for single epoch.
+               device: torch.device) -> Tuple[float, float]:
+    """Trains a PyTorch model for a single epoch.
 
-    Turns a target pytorch model to training mode and then
-    runs through all of the required training steps(forward,
-    calculate the loss function , optimize the zero grad, step 
-    backward, optimizer step)
+    Turns a target PyTorch model to training mode and then
+    runs through all of the required training steps (forward
+    pass, loss calculation, optimizer step).
 
     Args:
-        model      :     A pytorch model to be trained
-        dataloader :     A Dataloader instance for the model to be trained on.
-        loss_fn    :     A pytorch loss function to minimize
-        optimizer  :     A pytorch optimizer to help minimize the loss function
-        device     :     A target device to compute on (e.g. "cuda" or "cup")
-    
+    model: A PyTorch model to be trained.
+    dataloader: A DataLoader instance for the model to be trained on.
+    loss_fn: A PyTorch loss function to minimize.
+    optimizer: A PyTorch optimizer to help minimize the loss function.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+
     Returns:
-        A tuple of training loss and training accuracy metrics.
-        In the form  (train_loss, train_accuacy). For example:
+    A tuple of training loss and training accuracy metrics.
+    In the form (train_loss, train_accuracy). For example:
 
-        (0.1232,0.4145)
+    (0.1112, 0.8743)
     """
-
-    # put model in train mode
-
+    # Put model in train mode
     model.train()
 
     # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
 
-    train_loss,train_acc =0,0
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
 
-    # Loop through the batch and dataloader
-
-    for batch, (X,y) in enumerate(dataloader):
-
-        # send data to target device
-
-        X,y=X.to(device) , y.to(device)
-
-        # 1. do forward pass 
+        # 1. Forward pass
         y_pred = model(X)
 
-        # 2. calculate the loss function
-        loss=loss_fn(y_pred,y)
-        train_loss += loss
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
 
-        # 3. opitimize the zero grad
+        # 3. Optimizer zero grad
         optimizer.zero_grad()
 
         # 4. Loss backward
         loss.backward()
 
-        # 5. optimizer step
-
+        # 5. Optimizer step
         optimizer.step()
 
-        # Calculate the accumulate accuracy metric across all batches
-
-        y_pred_class=torch.argmax(torch.softmax(y_pred,dim=1),dim=1)
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         train_acc += (y_pred_class == y).sum().item()/len(y_pred)
-    
-    # Adjust metrics to get average loss an accuracy per batch
 
-    train_loss=train_loss/len(dataloader)
-    train_acc=train_acc/len(dataloader)
-
-    return train_loss,train_acc
-
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
 
 def test_step(model: torch.nn.Module,
               dataloader: torch.utils.data.DataLoader,
               loss_fn: torch.nn.Module,
-              device:torch.device) -> Tuple[float,float]:
-    """Test a pytorch model for a single epoch, 
-    
-    Turns a target Pytorch model to "eval" mode and
-    then performs a forward pass on a testing dataset.
+              device: torch.device) -> Tuple[float, float]:
+    """Tests a PyTorch model for a single epoch.
+
+    Turns a target PyTorch model to "eval" mode and then performs
+    a forward pass on a testing dataset.
 
     Args:
-        model      :  A pytorch model to be tested
-        dataloader :  A DataLoader instance for the model to be tasted on.
-        loss_fn    :  A pytorch loss function to calculate the loss on the test data
-        device     :  A target device to compute on (e.g. "cuda" or "cpu")
+    model: A PyTorch model to be tested.
+    dataloader: A DataLoader instance for the model to be tested on.
+    loss_fn: A PyTorch loss function to calculate loss on the test data.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
 
     Returns:
-        A tuple of testing loss and testing accuracy metrics.
-        In the form (test_loss,test_accuracy). For example:
+    A tuple of testing loss and testing accuracy metrics.
+    In the form (test_loss, test_accuracy). For example:
 
-        (0.2354,0.2523)
+    (0.0223, 0.8985)
     """
-    # put the model in eval mode
+    # Put model in eval mode
     model.eval()
 
-    # Setup test loss and test_accuracy 
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
 
-    test_loss,test_acc=0,0
-
-    # Setup testing loss and test accuacy values
+    # Turn on inference context manager
     with torch.inference_mode():
         # Loop through DataLoader batches
-        for batch,(X,y) in enumerate(dataloader):
-            # send data to traget device
-            X,y =X.to(device),y.to(device)
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
 
             # 1. Forward pass
             test_pred_logits = model(X)
 
-            # 2. Calculate the loss function
-            loss=loss_fn(test_pred_logits,y)
-            test_loss +=loss
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
 
-            # 3. Calculate and accumulate accuracy
-            test_pred_labels=torch.argmax(torch.softmax(test_pred_logits,dim=1),dim=1)
-            test_acc += (test_pred_labels==y).sum().item()/len(test_pred_labels)
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
 
-        
     # Adjust metrics to get average loss and accuracy per batch
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
-    return test_loss, test_acc 
-
+    return test_loss, test_acc
 
 def train(model: torch.nn.Module,
           train_dataloader: torch.utils.data.DataLoader,
